c11f70f105f8d275b3dedaf0582be9afc6e18f62
Daniel Andrews <daniel.a.s.andrews@gmail.com>
2018-02-13 20:01:47 -0500

    fix enzyme config with typescript

--- src/testSetup.ts
+++
@@ -1,9 +0,0 @@
-import * as  Enzyme from 'enzyme';
-import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
-
-// React 16 Enzyme adapter
-Enzyme.configure({ adapter: new ReactSixteenAdapter() });
-// Make Enzyme functions available in all test files without importing
-global.shallow = Enzyme.shallow;
-global.render = Enzyme.render;
-global.mount = Enzyme.mount;
