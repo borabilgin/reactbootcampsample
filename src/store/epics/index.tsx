020bc8b19095cc4eaa0f7980491eac1f187138cc
Jaime Pokhoy <jaimepokhoy@gmail.com>
2018-02-21 22:45:35 -0500

    Lab Start

--- src/store/epics/index.tsx
+++ src/store/epics/index.tsx
@@ -1,19 +1,14 @@
-import * as constants from '../constants';
-import { ajax } from 'rxjs/observable/dom/ajax';
+import { MOVE } from '../constants';
+import { ajax } from 'rxjs/observable/dom/ajax';    // To issue response
 import { Move, win } from '../actions';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
-import { AjaxResponse } from 'rxjs/observable/dom/AjaxObservable';
+import 'rxjs/add/operator/mergeMap';                // Merge events into one stream
+import 'rxjs/add/operator/filter';                  // Filter out stream events
+import 'rxjs/add/operator/map';                     // Perform action on each item
+import { AjaxResponse } from 'rxjs/observable/dom/AjaxObservable';  // Type interface
 
-const url = 'https://bootcamp-todo-api.herokuapp.com/api/winner';
-// const url = 'http://localhost:8080/api/winner';
+const url = 'https://bootcamp-todo-api.herokuapp.com/api/winner';   // Server url
 
-export const rootEpic = (action$: any) =>
-    action$.ofType(constants.MOVE)
-        .mergeMap((action: Move) => 
-            ajax.post(url, action.history[action.history.length - 1])
-                .filter((res: AjaxResponse) => res.response !== null)
-                .map((winner: AjaxResponse) =>
-                    win(winner.response)
-                ));
+// Implement this epic
+// export const rootEpic = () => {
+//     return;
+// };
